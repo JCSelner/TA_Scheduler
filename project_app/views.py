9871ae9977d87b863e9377a6832a5c9445c932c5
ToastyyX <erick.cool321@icloud.com>
2024-04-20 20:46:01 -0500

    initial commit

--- project_app/views.py
+++ project_app/views.py
@@ -1,3 +1,47 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views import View
+from django.contrib.auth import authenticate, login, logout
+from datetime import datetime
+from project_app.models import User, Course, Assignment, Lab, Roles
+
 
 # Create your views here.
+
+
+class Courses(View):
+    def get(self, request):
+        pass
+
+    def post(self, request):
+        pass
+
+
+class Login(View):
+    def get(self, request):
+        return render(request, 'login.html')
+
+    def post(self, request):
+        username = request.POST.get('userID')
+        password = request.POST.get('password')
+
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            error_message = "Invalid username or password."
+            return render(request, 'login.html', {'error_message': error_message})
+
+
+class Home(View):
+    def get(self, request):
+        return render(request, 'home.html')
+
+    def post(self, request):
+        pass
+
+
+class Logout(View):
+    def get(self, request):
+        logout(request)
+        return redirect('login')
