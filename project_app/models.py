612b5a2daee3a6bca05d8407f0eb34a760a23f99
ToastyyX <erick.cool321@icloud.com>
2024-04-20 20:43:42 -0500

    initial commit

--- project_app/models.py
+++ project_app/models.py
@@ -1,3 +1,50 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Roles(models.TextChoices):
+    instructor = "I"
+    ta = "T"
+    admin = "A"
+
+
+class User(models.Model):
+    userID = models.CharField(max_length=20)
+    password = models.CharField(max_length=20)
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=20)
+    address = models.CharField(max_length=10)
+    role = models.CharField(max_length=1, choices=Roles.choices)
+    firstName = models.CharField(max_length=20)
+    lastName = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.firstName, self.lastName, self.email, self.phone, self.address
+
+
+class Course(models.Model):
+    courseName = models.CharField(max_length=20)
+    courseDescription = models.CharField(max_length=20)
+    courseID = models.IntegerField(primary_key=True)
+    courseSemester = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.courseName, self.courseSemester
+
+
+class Assignment(models.Model):
+    courseID = models.ForeignKey(Course, on_delete=models.CASCADE)
+    userID = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.courseID, self.userID
+
+
+class Lab(models.Model):
+    courseID = models.ForeignKey(Course, on_delete=models.CASCADE)
+    taID = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    labID = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.taID, self.courseID, self.labID
