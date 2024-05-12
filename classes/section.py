aa97c0e531b657742d7965cee400e8e60b03765b
Chubbles1 <tkdang@uwm.edu>
2024-05-11 21:35:03 -0700

    Revamped editSection method and tests

--- classes/section.py
+++ classes/section.py
@@ -1,33 +1,29 @@
 from project_app.models import Section, Course, User
 
 class SectionClass:
-    def __init__(self, sectionID=0, sectionType="Lab", course=Course(), taID=User()):
+    def __init__(self, sectionID=0, type="Lab", course=Course(), taID=User()):
         self.sectionID = sectionID
-        self.sectionType = sectionType
-        self.course=course
-        self.taID=taID
+        self.type = type
+        self.course = course
+        self.taID = taID
     def sectInDB(self, sectionID=""):
         sectionList = Section.objects.filter(sectionID=sectionID)
         return len(sectionList)==1
-    def createSection(self, sectionID="", sectionType="", course=Course(), taID=None):
+    def createSection(self, sectionID="", type="", course=Course(), taID=None):
         #Check for a unique sectionID
         if self.sectInDB(sectionID) or sectionID=="":
             return False
         #Check for valid section type
-        if sectionType != "Lab" and sectionType != "Lecture":
+        if type != "Lab" and type != "Lecture":
             return False
-        Section.objects.create(sectionID=sectionID, type=sectionType, course=course, taID=taID)
+        Section.objects.create(sectionID=sectionID, type=type, course=course, taID=taID)
         return True
-    def editSection(self, sectionID="", type=None, taID=None):
-        if not self.sectInDB(sectionID):
-            return False
-        section = Section.objects.get(sectionID=sectionID)
-        if type is None and taID is None:
-            return False
-        if type is not None:
-            section.type = type
-        if taID is not None:
-            section.taID = taID
+    def editSection(self, section, **kwargs):
+        for key, value in kwargs.items():
+            if hasattr(Section, key):
+                setattr(section, key, value)
+            else:
+                raise AttributeError(f"Attribute '{key}' does not exist in Section")
         section.save()
         return True
     def deleteSection(self, sectionID=""):
